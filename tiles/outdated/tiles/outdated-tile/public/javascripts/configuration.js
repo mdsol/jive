48896fcb9b0190ac835754d50dcbc3d34c80ce00
Tyler Ishikawa <tyishikawa@mdsol.com>
2016-06-30 19:17:44 -0400

    add options for link

--- tiles/outdated/tiles/outdated-tile/public/javascripts/configuration.js
+++ tiles/outdated/tiles/outdated-tile/public/javascripts/configuration.js
@@ -7,14 +7,19 @@
             config.data = {
                 title: "Recent Outdated Content",
                 numDocs: 10,
-                numDocsDefault: 10,
-                place: "sub"
+                place: "sub",
+                showLink: true,
+                linkText: "See More Outdated Content",
+                linkUrl: ""
             };
         };
         
         var title = document.getElementById("title");
         var numDocs = document.getElementById("num-docs");
         var radios = document.getElementsByName("place");
+        var showLink = document.getElementById("show-link");
+        var linkText = document.getElementById("link-text");
+        var linkUrl = document.getElementById("link-url");
 
         // populate the dialog with existing config value
         title.value = config.data.title;
@@ -25,13 +30,17 @@
                 break;
             }
         }
-
+        showLink.checked = config.data.showLink;
+        $("#link-options").toggle(showLink.checked);
+        linkText.value = config.data.linkText;
+        linkUrl.value = config.data.linkUrl;
+        gadgets.window.adjustHeight();
 
         function validate(data) {
             var valid = true;
-            var inputDivs = document.getElementsByClassName("jive-error-box");
-            for (div of inputDivs) {
-                div.className = "";
+            var inputs = document.getElementsByClassName("error-box");
+            for (var el of inputs) {
+                el.classList.remove("error-box");
             }
 
             numDocsVal = Number(data.numDocs.value);
@@ -40,25 +49,38 @@
                 showError(data.numDocs);
                 valid = false;
             }
+
+            if (data.showLink.checked && data.linkText.value === "") {
+                showError(data.linkText);
+                valid = false;
+            }
+
             return valid;
         }
         
         function showError(errInput) {
-            errInput.className += " error-box";
+            errInput.classList.add("error-box");
         }
 
         // update config object after clicking submit
         $("#btn-submit").click( function() {
             config.data.title = title.value;
-
+            config.data.numDocs = numDocs.value;
             for (var choice of radios) {
                 if (choice.checked) {
                     config.data.place = choice.value;
                     break;
                 }
             }
-            if (validate({title: title, numDocs: numDocs})) {
-                config.data.numDocs = numDocs.value;
+            config.data.showLink = showLink.checked;
+            config.data.linkText = linkText.value;
+            config.data.linkUrl = linkUrl.value;
+            var checkData = {
+                numDocs: numDocs,
+                showLink: showLink,
+                linkText: linkText
+            }
+            if (validate(checkData)) {
                 jive.tile.close(config, {} );
             } else {
                 gadgets.window.adjustHeight();
@@ -67,3 +89,10 @@
     });
 
 })();
+
+$(document).ready(function() {
+    $("#show-link").change(function() {
+        $("#link-options").toggle();
+        gadgets.window.adjustHeight();
+    });
+});
