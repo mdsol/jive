9f9e5f62d234e06dd3fa758b010884c8c4efa1b9
Tyler Ishikawa <tyishikawa@mdsol.com>
2017-01-03 16:54:05 -0500

    convert code in view.js into an IIFE

--- tiles/ask-a-question/tiles/ask-a-question/public/javascripts/view.js
+++ tiles/ask-a-question/tiles/ask-a-question/public/javascripts/view.js
@@ -1,206 +1,208 @@
-// flag for recording time and logging to console
-var timer = false;
-var start, lap;
+(function() {
+    // flag for recording time and logging to console
+    var timer = false;
+    var start, lap;
 
-// post a question URL
-var questionUrl = "/discussion/create.jspa?question=true";
+    // post a question URL
+    var questionUrl = "/discussion/create.jspa?question=true";
 
-// array of placeIDs of subplaces prepended with "/places/"
-var places = [];
+    // array of placeIDs of subplaces prepended with "/places/"
+    var places = [];
 
-// array of question search results
-var results = [];
+    // array of question search results
+    var results = [];
 
-jive.tile.onOpen(function(config, options) {
-    gadgets.window.adjustHeight();
+    jive.tile.onOpen(function(config, options) {
+        gadgets.window.adjustHeight();
 
-    // set defaults for config
-    config.numResults = config.numResults || 10;
-    config.qType = config.qType || "all";
-    config.place = config.place || "all";
+        // set defaults for config
+        config.numResults = config.numResults || 10;
+        config.qType = config.qType || "all";
+        config.place = config.place || "all";
 
-    jive.tile.getContainer(function(container) {
+        jive.tile.getContainer(function(container) {
 
-        places.push("/places/" + container.placeID);
-        if (config.place === "sub") {
-            getSubplaces(container.placeID);
-        }
-
-        $("#question-input").on("input", function(e) {
-            if (timer) {
-                start = Date.now();
+            places.push("/places/" + container.placeID);
+            if (config.place === "sub") {
+                getSubplaces(container.placeID);
             }
-            getQuestions($(this).val());
-        });
 
-        function getSubplaces(placeID) {
-            var reqSubspace = osapi.jive.corev3.places.get({
-                uri: "/places/" + placeID
+            $("#question-input").on("input", function(e) {
+                if (timer) {
+                    start = Date.now();
+                }
+                getQuestions($(this).val());
             });
-            reqSubspace.execute(function(res) {
-                if (res.error) {
-                    var code = res.error.code;
-                    var message = res.error.message;
-                    console.log(code + " " + message);
-                    // present the user with an appropriate error message
-                } else {
-                    var options = {
-                        count: 100, // most likely not more than 100
-                        filter: "type(space,project,group)"
+
+            function getSubplaces(placeID) {
+                var reqSubspace = osapi.jive.corev3.places.get({
+                    uri: "/places/" + placeID
+                });
+                reqSubspace.execute(function(res) {
+                    if (res.error) {
+                        var code = res.error.code;
+                        var message = res.error.message;
+                        console.log(code + " " + message);
+                        // present the user with an appropriate error message
+                    } else {
+                        var options = {
+                            count: 100, // most likely not more than 100
+                            filter: "type(space,project,group)"
+                        }
+                        res.getPlaces(options).execute(function(res) {
+                            if (res.error) {
+                                var code = res.error.code;
+                                var message = res.error.message;
+                                console.log(code + " " + message);
+                                // present the user with an appropriate error message
+                            } else {
+                                var resList = res.list;
+                                for (place of resList) {
+                                    places.push("/places/" + place.placeID);
+                                    getSubplaces(place.placeID);
+                                }
+                            }
+                        });
                     }
-                    res.getPlaces(options).execute(function(res) {
-                        if (res.error) {
-                            var code = res.error.code;
-                            var message = res.error.message;
-                            console.log(code + " " + message);
-                            // present the user with an appropriate error message
-                        } else {
-                            var resList = res.list;
-                            for (place of resList) {
-                                places.push("/places/" + place.placeID);
-                                getSubplaces(place.placeID);
+                });
+            }
+
+            function getQuestions(query, startIndex = 0) {
+
+                // hide the ask button
+                $("#ask").hide();
+
+                // hide no results message
+                $("#no-results-msg").hide();
+
+                // hide results if no query
+                if (query === "" || query === "\\") {
+                    clearResults();
+                    hideLoading();
+                    gadgets.window.adjustHeight();
+                    return;
+                }
+
+                // make room for loading icon
+                if ($("#result-list").children().length === 0) {
+                    gadgets.window.adjustHeight( gadgets.window.getHeight() + $("#loading").height() * 1.5);
+                }
+                showLoading();
+
+                options = {
+                    search: query,
+                    type: "discussion",
+                    count: 100,
+                    fields: "question,resolved,subject,author.displayName,published,iconCss",
+                    startIndex: startIndex
+                }
+                if (config.place !== "all") {
+                    options.place = places.join(",");
+                }
+
+                osapi.jive.corev3.contents.get(options).execute(function(res) {
+                    if (res.error) {
+                        var code = res.error.code;
+                        var message = res.error.message;
+                        console.log(code + " " + message);
+                        // present the user with an appropriate error message
+                    } else {
+                        if (timer) {
+                            lap = Date.now();
+                            console.log("query took " + (lap - start) + " ms");
+                        }
+                        if (query !== $("#question-input").val()) {
+                            return;
+                        }
+
+                        for (var r of res.list) {
+                            if (r.question && (config.qType === "all" || r.resolved.indexOf(config.qType) !== -1)) {
+                                results.push(r);
+                            }
+                            if (results.length === config.numResults) {
+                                break;
                             }
                         }
-                    });
-                }
-            });
-        }
 
-        function getQuestions(query, startIndex = 0) {
+                        if (res.links && res.links.next && results.length < config.numResults) {
+                            getQuestions(query, startIndex + 100);
+                        } else {
+                            showResults();
+                        }
+                    }
+                });
+            }
 
-            // hide the ask button
-            $("#ask").hide();
-
-            // hide no results message
-            $("#no-results-msg").hide();
-
-            // hide results if no query
-            if (query === "" || query === "\\") {
+            function showResults() {
+                var ul = document.getElementById("result-list");
                 clearResults();
-                hideLoading();
-                gadgets.window.adjustHeight();
-                return;
-            }
 
-            // make room for loading icon
-            if ($("#result-list").children().length === 0) {
-                gadgets.window.adjustHeight( gadgets.window.getHeight() + $("#loading").height() * 1.5);
-            }
-            showLoading();
+                for (var r of results) {
+                    var li = document.createElement("li");
+                    var a = document.createElement("a");
+                    a.setAttribute("target", "_top");
+                    a.setAttribute("href", r.resources.html.ref);
+                    var icon = document.createElement("span");
+                    icon.classList.add(r.iconCss, "jive-icon-med");
+                    var subj = document.createElement("span");
+                    subj.classList.add("lnk");
+                    subj.appendChild( document.createTextNode(r.subject) );
+                    var em = document.createElement("em");
+                    emText = document.createTextNode("asked by " + r.author.displayName + " on " + formatDate(r.published));
+                    em.appendChild(emText);
 
-            options = {
-                search: query,
-                type: "discussion",
-                count: 100,
-                fields: "question,resolved,subject,author.displayName,published,iconCss",
-                startIndex: startIndex
-            }
-            if (config.place !== "all") {
-                options.place = places.join(",");
-            }
-            
-            osapi.jive.corev3.contents.get(options).execute(function(res) {
-                if (res.error) {
-                    var code = res.error.code;
-                    var message = res.error.message;
-                    console.log(code + " " + message);
-                    // present the user with an appropriate error message
-                } else {
-                    if (timer) {
-                        lap = Date.now();
-                        console.log("query took " + (lap - start) + " ms");
-                    }
-                    if (query !== $("#question-input").val()) {
-                        return;
-                    }
-
-                    for (var r of res.list) {
-                        if (r.question && (config.qType === "all" || r.resolved.indexOf(config.qType) !== -1)) {
-                            results.push(r);
-                        }
-                        if (results.length === config.numResults) {
-                            break;
-                        }
-                    }
-
-                    if (res.links && res.links.next && results.length < config.numResults) {
-                        getQuestions(query, startIndex + 100);
-                    } else {
-                        showResults();
-                    }
+                    a.appendChild(icon);
+                    a.appendChild(subj);
+                    a.appendChild(em);
+                    li.appendChild(a);
+                    ul.appendChild(li);
                 }
-            });
-        }
 
-        function showResults() {
-            var ul = document.getElementById("result-list");
-            clearResults();
+                // change link for ask button
+                var parser = document.createElement("a");
+                parser.href = container.resources.html.ref;
+                var url = parser.origin + questionUrl;
+                url += "&containerType=" + container.typeCode;
+                url += "&containerID=" + container.id;
+                url += "&subject=" + $("#question-input").val();
+                $("#ask").attr("href", url);
 
-            for (var r of results) {
-                var li = document.createElement("li");
-                var a = document.createElement("a");
-                a.setAttribute("target", "_top");
-                a.setAttribute("href", r.resources.html.ref);
-                var icon = document.createElement("span");
-                icon.classList.add(r.iconCss, "jive-icon-med");
-                var subj = document.createElement("span");
-                subj.classList.add("lnk");
-                subj.appendChild( document.createTextNode(r.subject) );
-                var em = document.createElement("em");
-                emText = document.createTextNode("asked by " + r.author.displayName + " on " + formatDate(r.published));
-                em.appendChild(emText);
+                $("#ask").css("display", "inline-block");
 
-                a.appendChild(icon);
-                a.appendChild(subj);
-                a.appendChild(em);
-                li.appendChild(a);
-                ul.appendChild(li);
+                if (results.length === 0) {
+                    $("#no-results-msg").show();
+                }
+
+                results = [];
+                hideLoading();
+
+                if (timer) {
+                    console.log("creating nodes took " + (Date.now() - lap) + " ms");
+                }
+                gadgets.window.adjustHeight();
             }
 
-            // change link for ask button
-            var parser = document.createElement("a");
-            parser.href = container.resources.html.ref;
-            var url = parser.origin + questionUrl;
-            url += "&containerType=" + container.typeCode;
-            url += "&containerID=" + container.id;
-            url += "&subject=" + $("#question-input").val();
-            $("#ask").attr("href", url);
-
-            $("#ask").css("display", "inline-block");
-
-            if (results.length === 0) {
-                $("#no-results-msg").show();
+            function formatDate(dateStr) {
+                var date = new Date(dateStr);
+                return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
             }
 
-            results = [];
-            hideLoading();
-
-            if (timer) {
-                console.log("creating nodes took " + (Date.now() - lap) + " ms");
+            function showLoading() {
+                $("#loading").show();
+                $("#result-list").css("opacity", 0.5);
             }
-            gadgets.window.adjustHeight();
-        }
 
-        function formatDate(dateStr) {
-            var date = new Date(dateStr);
-            return (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear();
-        }
-
-        function showLoading() {
-            $("#loading").show();
-            $("#result-list").css("opacity", 0.5);
-        }
-
-        function hideLoading() {
-            $("#loading").hide();
-            $("#result-list").css("opacity", 1);
-        }
-
-        function clearResults() {
-            var ul = document.getElementById("result-list");
-            while (ul.hasChildNodes()) {
-                ul.removeChild(ul.lastChild);
+            function hideLoading() {
+                $("#loading").hide();
+                $("#result-list").css("opacity", 1);
             }
-        }
+
+            function clearResults() {
+                var ul = document.getElementById("result-list");
+                while (ul.hasChildNodes()) {
+                    ul.removeChild(ul.lastChild);
+                }
+            }
+        });
     });
-});
+})();
