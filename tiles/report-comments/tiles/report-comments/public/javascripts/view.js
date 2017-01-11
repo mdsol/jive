e88135a191d39e9403cc621ea09e732d868f455d
Tyler Ishikawa <tyishikawa@mdsol.com>
2017-01-11 10:59:57 -0500

    search for and link discussion about current page

--- tiles/report-comments/tiles/report-comments/public/javascripts/view.js
+++ tiles/report-comments/tiles/report-comments/public/javascripts/view.js
@@ -23,9 +23,29 @@ function onReady(tileConfig,tileOptions,viewer,container) {
     })[0];
 
     console.log(page.name);
+    console.log(container);
+    var queryStr = "/search/contents?filter=type(discussion)";
+    queryStr += "&filter=search(" + page.name + ")";
+    console.log(container.placeID);
+    console.log(queryStr);
+
+    osapi.jive.core.get({
+      v: "v3",
+      href: queryStr
+    }).execute(function(resp) {
+      console.log(resp);
+      var discList = resp.list.filter(function(x) {return x.subject === page.name});
+      var titleText = "Contents ("
+                      + (discList.length === 0 ? 0 : discList[0].replyCount)
+                      + ")";
+      var anchor = $("<a>", { text: titleText,
+                              href: discList[0].resources.html.ref });
+
+      $("#title-text").append(discList.length === 0 ? titleText : anchor);
+      app.resize();
+    });
   });
 
-  app.resize();
 } // end function
 
 //************************************************************************
